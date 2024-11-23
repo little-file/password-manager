c17e49b028d5849d3afa138e061d9dd5125af1a9
little-file <fakeaiqqff@gmail.com>
2024-11-23 09:41:26 +0300

    I finished writing generate.py

---
+++ src/generate.py
@@ -0,0 +1,12 @@
+import random
+import string
+def main():
+    lowercase_letters = string.ascii_lowercase
+    uppercase_letters = string.ascii_uppercase
+    numbers = string.digits
+    special_character = "!'^+%&/()=?_>£<#$½¾{[]} |*-"
+    leght = int(input("enter the leght: "))
+    all_character= lowercase_letters + uppercase_letters + numbers + special_character
+    password = "".join(random.choices(all_character , k= leght))
+    print(password)
+main()
